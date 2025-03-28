a67654a1295faded5e1dfefc896cec959ec25fe8
vetlesho <vetlesho@stud.ntnu.no>
2025-03-28 15:13:19 +0100

    Create listingRepo and listingService

--- backend/SpringBackend/src/main/java/edu/ntnu/SpringBackend/repository/ListingRepository.java
+++ backend/SpringBackend/src/main/java/edu/ntnu/SpringBackend/repository/ListingRepository.java
@@ -2,6 +2,7 @@ package edu.ntnu.SpringBackend.repository;
 
 import edu.ntnu.SpringBackend.model.Listing;
 import edu.ntnu.SpringBackend.model.User;
+import edu.ntnu.SpringBackend.model.enums.Category;
 import edu.ntnu.SpringBackend.model.enums.ListingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -12,4 +13,5 @@ public interface ListingRepository extends JpaRepository<Listing, Integer> {
   List<Listing> findBySeller(User user);
   Optional<Listing> findById(int id);
   List<Listing> findByStatus(ListingStatus status);
+  List<Listing> findByCategory(Category category);
 }
