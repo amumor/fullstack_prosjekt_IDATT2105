99e1b2d45b2cd0d7dd183878e4b1eed4a0e93607
Madeleine Negård <madelne@stud.ntnu.no>
2025-03-30 16:32:10 +0200

    Added style to CredentialsBox

--- frontend/VueFrontend/src/components/CredentialsBox.vue
+++ frontend/VueFrontend/src/components/CredentialsBox.vue
@@ -21,7 +21,8 @@ const login = () => {
 
 <template>
 <div class="credentials-box">
-  <template v-if="!props.hasUser">
+  <h2 class="logo-header">FIND.no</h2>
+  <template v-if="props.hasUser">
     <div class="login">
       <h2>Log in</h2>
       <div class="fields">
@@ -57,5 +58,99 @@ const login = () => {
 </template>
 
 <style scoped>
+.credentials-box {
+  background: white;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+  font-family: 'Inter', sans-serif;
+
+  width: 40%;
+  height: 70%;
+  padding: 40px;
+  border-radius: 10px;
+  margin: 70px auto;
+  position: relative;
+}
+
+/* Logo */
+.logo-header {
+  font-size: 26px;
+  font-weight: 600;
+  color: #333333;
+  text-align: left;
+
+  margin: 0;
+}
+
+/* Title */
+h2 {
+  font-size: 24px;
+  color: #333333;
+  font-weight: 500;
+
+  text-align: left;
+  margin-bottom: 20px;
+  margin-top: 60px;
+}
+
+/* Input fields */
+.fields input {
+  font-size: 16px;
+
+  width: 60%;
+  padding: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+/* Log in and register button */
+.fields a, .fields button {
+  background: #1C64FF;
+  color: white;
+  font-size: 16px;
+  transition: background 0.3s ease;
+
+  text-align: center;
+  width: 66%;
+  display: block;
+  padding: 10px;
+  border-radius: 5px;
+  margin-top: 10px;
+  border: none;
+}
+
+.fields a {
+  display: block;
+  text-decoration: none;
+  width: 60%;
+}
+
+
+.fields a:hover, .fields button:hover {
+  background: #0056b3;
+}
+
+/* Router links */
+.to-sign-up, .to-login {
+  font-size: 14px;
+  color: #333333;
+
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  margin-top: 10px;
+}
+
+.to-sign-up button, .to-login button {
+  background: none;
+  border: none;
+  color: #d97706;
+  font-size: 14px;
+  cursor: pointer;
+}
+
+.to-sign-up button:hover, .to-login button:hover {
+  text-decoration: underline;
+}
 
 </style>
