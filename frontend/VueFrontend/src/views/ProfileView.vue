07a18d2dc156cb962f09a568c83be4c6f455ecbc
Madeleine Negård <madelne@stud.ntnu.no>
2025-03-31 13:52:45 +0200

    Font

--- frontend/VueFrontend/src/views/ProfileView.vue
+++ frontend/VueFrontend/src/views/ProfileView.vue
@@ -48,12 +48,15 @@ const getInitials = (name) => {
 </template>
 
 <style scoped>
+
+/* Profile initials */
 .profile-icon {
   background-color: #D9D9D9;
   border-radius: 50%;
   color: #333333;
   font-size: 30px;
   text-transform: uppercase;
+  font-family: 'Inter', sans-serif;
 
   display: flex;
   justify-content: center;
@@ -67,6 +70,7 @@ const getInitials = (name) => {
   margin: 30px 0 0 70px;
 }
 
+/* Title */
 h2 {
   font-size: 24px;
   font-family: 'Inter', sans-serif;
@@ -75,6 +79,7 @@ h2 {
   margin: 30px 0 0 30px;
 }
 
+/* Description */
 h1 {
   font-size: 16px;
   font-family: 'Inter', sans-serif;
