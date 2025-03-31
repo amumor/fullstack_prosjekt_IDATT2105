5f2df14083e1cd3120d16268b465809a8e2c4a3c
Madeleine Negård <madelne@stud.ntnu.no>
2025-03-31 15:15:50 +0200

    Router to ListingView

--- frontend/VueFrontend/src/router/index.js
+++ frontend/VueFrontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
 import ProfileView from '@/views/ProfileView.vue'
+import ListingView from '@/views/ListingView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,11 @@ const router = createRouter({
       name: 'profile',
       component: ProfileView
     },
+    {
+      path: '/listing',
+      name: 'listing',
+      component: ListingView
+    }
   ],
 })
 
