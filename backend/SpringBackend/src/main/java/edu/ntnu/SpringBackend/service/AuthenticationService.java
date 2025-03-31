c09c20ace225faa422f9e8fcd8b0ceb66bccd523
Amund Mørk <amundmor@stud.ntnu.no>
2025-03-31 12:14:41 +0200

    update and fix authenticationService

--- backend/SpringBackend/src/main/java/edu/ntnu/SpringBackend/service/AuthenticationService.java
+++ backend/SpringBackend/src/main/java/edu/ntnu/SpringBackend/service/AuthenticationService.java
@@ -6,12 +6,14 @@ import edu.ntnu.SpringBackend.dto.AuthenticationRequest;
 import edu.ntnu.SpringBackend.dto.AuthenticationResponse;
 import edu.ntnu.SpringBackend.dto.RegisterRequest;
 import edu.ntnu.SpringBackend.model.User;
+import edu.ntnu.SpringBackend.model.enums.Role;
 import edu.ntnu.SpringBackend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -29,33 +31,48 @@ public class AuthenticationService {
 
     public AuthenticationResponse register(RegisterRequest request) {
         logger.info("Handling register request...");
-        var user = User.builder() // TODO: dobble check details in login form
+
+        if (repository.findByEmail(request.getEmail()).isPresent()) {
+            logger.info("> Email already present in database.");
+            throw new IllegalArgumentException("User with email " + request.getEmail() + " already exists.");
+        }
+
+        var user = User.builder()
                 .email(request.getEmail())
                 .firstName(request.getFirstName())
                 .lastName(request.getLastName())
                 .password(passwordEncoder.encode(request.getPassword()))
                 .phoneNumber(request.getPhoneNumber())
+                .role(Role.USER)
                 .build();
+
+        logger.info("> Saving user to the database.");
+        repository.save(user);
+        logger.info("> Generating jwt token for user [" + request.getEmail() + "].");
         var jwtToken = jwtService.generateToken(user);
+        logger.info("> User registered.");
         return AuthenticationResponse.builder().token(jwtToken).build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
         logger.info("Handling authentication request...");
-        logger.info("---- email: " + request.getEmail());
-        logger.info("---- password: " + request.getPassword());
+        logger.info("> Email: " + request.getEmail());
+
+        try {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         request.getEmail(),
                         request.getPassword()
                 )
         );
-        logger.info("request email: " + request.getEmail());
+        } catch (AuthenticationException ae) {
+            logger.error("> Authentication failed for email: " + request.getEmail() + ", " + ae.getMessage());
+            throw ae;
+        }
+
         var user = repository.findByEmail(request.getEmail())
-                .orElseThrow(() -> new NoSuchElementException("User not found with email: " + request.getEmail()));
-
-
-        logger.info("User [" + user.getEmail() + "] has been successfully authenticated");
+                .orElseThrow(() -> new NoSuchElementException("---- User not found with email: " + request.getEmail()));
+        logger.info("> User [" + user.getEmail() + "] has been successfully authenticated");
         var jwtToken = jwtService.generateToken(user);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
