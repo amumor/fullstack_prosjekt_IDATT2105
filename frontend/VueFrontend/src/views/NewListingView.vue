296af38ab3562075f07fd18ea594a24a2296de97
Madeleine Negård <madelne@stud.ntnu.no>
2025-04-01 14:32:17 +0200

    NewListingView page

---
+++ frontend/VueFrontend/src/views/NewListingView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import NewListingComponent from '@/components/Listing/NewListingComponent.vue'
+import Navbar from '@/components/Navbar.vue'
+
+</script>
+
+<template>
+  <Navbar :isLoggedIn="true"/>
+  <NewListingComponent />
+</template>
+
+<style scoped>
+
+</style>
