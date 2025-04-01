661e533455f454a3d06d75b0873064afeb717b6c
Madeleine Negård <madelne@stud.ntnu.no>
2025-04-01 15:53:31 +0200

    Router to MyListingsView

--- frontend/VueFrontend/src/router/index.js
+++ frontend/VueFrontend/src/router/index.js
@@ -4,6 +4,7 @@ import LoginView from '@/views/LoginView.vue'
 import ProfileView from '@/views/ProfileView.vue'
 import ListingView from '@/views/ListingView.vue'
 import NewListingView from '@/views/NewListingView.vue'
+import MyListingsView from '@/views/MyListingsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,11 @@ const router = createRouter({
       path: '/newListing',
       name: 'newListing',
       component: NewListingView
+    },
+    {
+      path: '/profile/myListings',
+      name: 'myListings',
+      component: MyListingsView
     }
   ],
 })
