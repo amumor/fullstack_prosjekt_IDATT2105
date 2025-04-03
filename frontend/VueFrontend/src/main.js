ccfacc993b296460b48357de4d30d8a6f988202f
Madeleine Negård <madelne@stud.ntnu.no>
2025-04-03 17:05:49 +0200

    Add vue-map dependency

--- frontend/VueFrontend/src/main.js
+++ frontend/VueFrontend/src/main.js
@@ -3,6 +3,10 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router/index.js'
+import 'leaflet/dist/leaflet.css';
+import 'vue-map-ui/dist/normalize.css';
+import 'vue-map-ui/dist/style.css';
+import 'vue-map-ui/dist/theme-all.css';
 
 const app = createApp(App)
 
