3b7513ef838efb98fe07616a60c0c4ff4bc636bd
Amund Mørk <amundmor@stud.ntnu.no>
2025-04-09 10:11:34 +0200

    commit to keep before refactoring

--- frontend/VueFrontend/src/views/HomeView.vue
+++ frontend/VueFrontend/src/views/HomeView.vue
@@ -1,70 +1,105 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue'
 import ListingPreviewComponent from '@/components/listing/ListingPreviewComponent.vue'
+import {getListingSuggestions} from "@/services/ListingService.js";
+import {userStore} from "@/stores/user.js";
+import {isTokenExpired} from "@/services/TokenService.js";
+import {getAllCategories} from "@/services/CategoryService.js";
+import {ref} from "vue";
+import router from "@/router/index.js";
 
 document.body.style.backgroundColor = "#ffffff";
 
-const searchFunction = () => {};
+const searchFunction = () => {
+};
 
 //Get images from backend
-const listings = [
-  {
-    id: 1,
-    price: '100 mill',
-    image: 'https://iqboatlifts.com/wp-content/uploads/2018/06/Yacht-vs-Boat-Whats-the-Difference-Between-the-Two-1024x571.jpg',
-    town: 'Elverum',
-    title: 'Big boat 1',
-  },
-  {
-    id: 2,
-    price: '60 mill',
-    image: 'https://iqboatlifts.com/wp-content/uploads/2018/06/Yacht-vs-Boat-Whats-the-Difference-Between-the-Two-1024x571.jpg',
-    town: 'Baerum',
-    title: 'Big Boat 2',
-  },
-];
+// const listings = [
+//   {
+//     id: 1,
+//     price: '100 mill',
+//     image: 'https://iqboatlifts.com/wp-content/uploads/2018/06/Yacht-vs-Boat-Whats-the-Difference-Between-the-Two-1024x571.jpg',
+//     town: 'Elverum',
+//     title: 'Big boat 1',
+//   },
+//   {
+//     id: 2,
+//     price: '60 mill',
+//     image: 'https://iqboatlifts.com/wp-content/uploads/2018/06/Yacht-vs-Boat-Whats-the-Difference-Between-the-Two-1024x571.jpg',
+//     town: 'Baerum',
+//     title: 'Big Boat 2',
+//   },
+// ];
 
-//Get categories from backend
-const categories = [
-  { id: 1, name: 'Boats' },
-  { id: 2, name: 'Cars' },
-  { id: 3, name: 'Motorcycles' },
-  { id: 4, name: 'Real Estate' },
-];
+// //Get categories from backend
+// const categories = [
+//   {id: 1, name: 'Boats'},
+//   {id: 2, name: 'Cars'},
+//   {id: 3, name: 'Motorcycles'},
+//   {id: 4, name: 'Real Estate'},
+// ];
+
+const user = userStore()
+const categories = ref([]);
+const listings = ref([]);
+try {
+  const token = user.token;
+  if (token === null || token === undefined || token === '') {
+    // If user is not logged in, get lsitings withtout token
+    const listingResponse = await getListingSuggestions({page: 1, size: 10});
+    const categoryResponse = await getAllCategories();
+    listings.value = listingResponse.listings;
+    categories.value = categoryResponse.categories;
+
+  } else {
+    if (isTokenExpired(token)) {
+      user.logout();
+      router.push("/");
+    } else {
+      // If user is logged in, get listings from suggestions with token
+      const listingResponse = await getListingSuggestions({page: 1, size: 10}, token);
+      const categoryResponse = await getAllCategories(token);
+      listings.value = listingResponse.listings;
+      categories.value = categoryResponse.categories;
+    }
+  }
+} catch (error) {
+  console.error(error);
+}
 </script>
 
 <template>
-<Navbar />
-<div class="display-page-container">
+  <Navbar/>
+  <div class="display-page-container">
 
-  <!-- Search bar -->
-  <div class="search-container">
-    <input type="text" class="search-input" placeholder="Search for listings..." id="searchInput">
-    <button class="search-btn" @click="searchFunction">Search</button>
-    <button class="map-btn">
-      <router-link to="/map" id="router-link">Map</router-link>
-    </button>
-  </div>
+    <!-- Search bar -->
+    <div class="search-container">
+      <input type="text" class="search-input" placeholder="Search for listings..." id="searchInput">
+      <button class="search-btn" @click="searchFunction">Search</button>
+      <button class="map-btn">
+        <router-link to="/map" id="router-link">Map</router-link>
+      </button>
+    </div>
 
-  <!-- Categories -->
-  <div class="category-container">
-    <div v-for="category in categories" :key="category.id">
-      <button id="category-btn">{{ category.name }}</button>
+    <!-- Categories -->
+    <div class="category-container">
+      <div v-for="category in categories" :key="category.id">
+        <button id="category-btn">{{ category.name }}</button>
+      </div>
+    </div>
+
+    <!-- Listings -->
+    <div class="listings">
+      <div v-for="listing in listings" :key="listing.id">
+        <ListingPreviewComponent
+            :id="listing.id"
+            :image="listing.image"
+            :price="listing.price"
+            :town="listing.town"
+            :title="listing.title"/>
+      </div>
     </div>
   </div>
-
-  <!-- Listings -->
-  <div class="listings">
-    <div v-for="listing in listings" :key="listing.id">
-      <ListingPreviewComponent
-        :id="listing.id"
-        :image="listing.image"
-        :price="listing.price"
-        :town="listing.town"
-        :title="listing.title" />
-    </div>
-  </div>
-</div>
 </template>
 
 <style scoped>
