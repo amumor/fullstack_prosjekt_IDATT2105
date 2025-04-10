86d11f0c90cd0511beb4980e8f67c70508744d7c
Amund Mørk <amundmor@stud.ntnu.no>
2025-04-10 10:20:45 +0200

    add printouts in /all

--- backend/SpringBackend/src/main/java/edu/ntnu/SpringBackend/controller/CategoryController.java
+++ backend/SpringBackend/src/main/java/edu/ntnu/SpringBackend/controller/CategoryController.java
@@ -5,11 +5,15 @@ import edu.ntnu.SpringBackend.dto.CategoryCreationRequestDTO;
 import edu.ntnu.SpringBackend.dto.CategoryListResponseDTO;
 import edu.ntnu.SpringBackend.dto.CategoryResponseDTO;
 import edu.ntnu.SpringBackend.mapper.CategoryMapper;
+import edu.ntnu.SpringBackend.model.Category;
 import edu.ntnu.SpringBackend.model.User;
 import edu.ntnu.SpringBackend.service.CategoryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
+import java.util.List;
 import java.util.UUID;
+
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,91 +42,94 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/category")
 @RequiredArgsConstructor
 public class CategoryController {
-  private final CategoryService categoryService;
-  private final Logger logger = LoggerFactory.getLogger(CategoryController.class);
-  private final CategoryMapper categoryMapper;
+    private final CategoryService categoryService;
+    private final Logger logger = LoggerFactory.getLogger(CategoryController.class);
+    private final CategoryMapper categoryMapper;
 
-  /**
-   * Creates a new category.
-   * This endpoint is secured and requires the user to have the ADMIN role.
-   *
-   * @param user    The authenticated user making the request.
-   * @param request The request body containing category creation details.
-   * @return A response entity containing the created category details.
-   */
-  @PreAuthorize("hasRole('ADMIN')")
-  @PostMapping("/create")
-  @Operation(summary = "Create a new category", security = @SecurityRequirement(name = "bearerAuth"))
-  public ResponseEntity<CategoryResponseDTO> create(
-          @AuthenticationPrincipal User user,
-          @RequestBody CategoryCreationRequestDTO request
-  ) {
-    logger.info("POST Request received on [/api/v1/category/create]");
-    return ResponseEntity.ok(categoryMapper.toDto(categoryService.add(request, user)));
-  }
+    /**
+     * Creates a new category.
+     * This endpoint is secured and requires the user to have the ADMIN role.
+     *
+     * @param user    The authenticated user making the request.
+     * @param request The request body containing category creation details.
+     * @return A response entity containing the created category details.
+     */
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/create")
+    @Operation(summary = "Create a new category", security = @SecurityRequirement(name = "bearerAuth"))
+    public ResponseEntity<CategoryResponseDTO> create(
+            @AuthenticationPrincipal User user,
+            @RequestBody CategoryCreationRequestDTO request
+    ) {
+        logger.info("POST Request received on [/api/v1/category/create]");
+        return ResponseEntity.ok(categoryMapper.toDto(categoryService.add(request, user)));
+    }
 
-  /**
-   * Deletes a category by its ID.
-   * This endpoint is secured and requires the user to have the ADMIN role.
-   *
-   * @param user The authenticated user making the request.
-   * @param id   The ID of the category to be deleted.
-   * @return A response entity with no content if the deletion was successful.
-   */
-  @PreAuthorize("hasRole('ADMIN')")
-  @DeleteMapping("/delete/{id}")
-  @Operation(summary = "Delete a category", security = @SecurityRequirement(name = "bearerAuth"))
-  public ResponseEntity<Void> delete(
-          @AuthenticationPrincipal User user,
-          @PathVariable UUID id
-  ) {
-    logger.info("DELETE request received on [/api/v1/category/delete/{}]", id);
-    categoryService.delete(id, user);
-    return ResponseEntity.noContent().build();
-  }
+    /**
+     * Deletes a category by its ID.
+     * This endpoint is secured and requires the user to have the ADMIN role.
+     *
+     * @param user The authenticated user making the request.
+     * @param id   The ID of the category to be deleted.
+     * @return A response entity with no content if the deletion was successful.
+     */
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Delete a category", security = @SecurityRequirement(name = "bearerAuth"))
+    public ResponseEntity<Void> delete(
+            @AuthenticationPrincipal User user,
+            @PathVariable UUID id
+    ) {
+        logger.info("DELETE request received on [/api/v1/category/delete/{}]", id);
+        categoryService.delete(id, user);
+        return ResponseEntity.noContent().build();
+    }
 
-  /**
-   * Retrieves a category by its name.
-   * This endpoint is available to all users.
-   *
-   * @param name The name of the category to be retrieved.
-   * @return A response entity containing the category details.
-   */
-  @GetMapping("/name/{name}")
-  @Operation(summary = "Get a category by name")
-  public ResponseEntity<CategoryResponseDTO> getByName(
-          @PathVariable String name
-  ) {
-    logger.info("GET request received on [/api/v1/category/name/{}]", name);
-    return ResponseEntity.ok(categoryMapper.toDto(categoryService.getByName(name)));
-  }
+    /**
+     * Retrieves a category by its name.
+     * This endpoint is available to all users.
+     *
+     * @param name The name of the category to be retrieved.
+     * @return A response entity containing the category details.
+     */
+    @GetMapping("/name/{name}")
+    @Operation(summary = "Get a category by name")
+    public ResponseEntity<CategoryResponseDTO> getByName(
+            @PathVariable String name
+    ) {
+        logger.info("GET request received on [/api/v1/category/name/{}]", name);
+        return ResponseEntity.ok(categoryMapper.toDto(categoryService.getByName(name)));
+    }
 
-  /**
-   * Retrieves a category by its ID.
-   * This endpoint is available to all users.
-   *
-   * @param id The ID of the category to be retrieved.
-   * @return A response entity containing the category details.
-   */
-  @GetMapping("/id/{id}")
-  @Operation(summary = "Get a category by ID")
-  public ResponseEntity<CategoryResponseDTO> getById(
-          @PathVariable UUID id
-  ) {
-    logger.info("GET request received on [/api/v1/category/id/{}]", id);
-    return ResponseEntity.ok(categoryMapper.toDto(categoryService.getById(id)));
-  }
+    /**
+     * Retrieves a category by its ID.
+     * This endpoint is available to all users.
+     *
+     * @param id The ID of the category to be retrieved.
+     * @return A response entity containing the category details.
+     */
+    @GetMapping("/id/{id}")
+    @Operation(summary = "Get a category by ID")
+    public ResponseEntity<CategoryResponseDTO> getById(
+            @PathVariable UUID id
+    ) {
+        logger.info("GET request received on [/api/v1/category/id/{}]", id);
+        return ResponseEntity.ok(categoryMapper.toDto(categoryService.getById(id)));
+    }
 
-  /**
-   * Retrieves all categories.
-   * This endpoint is available to all users.
-   *
-   * @return A response entity containing a list of all categories.
-   */
-  @GetMapping("/all")
-  @Operation(summary = "Get all categories")
-  public ResponseEntity<CategoryListResponseDTO> getAll() {
-    logger.info("GET request received on [/api/v1/category/all]");
-    return ResponseEntity.ok(categoryMapper.toDto(categoryService.getAll()));
-  }
+    /**
+     * Retrieves all categories.
+     * This endpoint is available to all users.
+     *
+     * @return A response entity containing a list of all categories.
+     */
+    @GetMapping("/all")
+    @Operation(summary = "Get all categories")
+    public ResponseEntity<CategoryListResponseDTO> getAll() {
+        logger.info("GET request received on [/api/v1/category/all]");
+        List<Category> categories = categoryService.getAll();
+        logger.info("Categories found: {}", categories.toString());
+        CategoryListResponseDTO categoryListResponseDTO = categoryMapper.toDto(categories);
+        return ResponseEntity.ok(categoryListResponseDTO);
+    }
 }
